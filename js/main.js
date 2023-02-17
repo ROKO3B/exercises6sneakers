b13b34fd36ca5d5a1e42a0875c416a44d042ca03
roko3 <rokorokokoro3@gmail.com>
2023-02-17 15:30:59 +0900

    20230217sp

--- js/main.js
+++ js/main.js
@@ -25,6 +25,35 @@ $(function() {
     return false;
   });
   
-  
+  /*スライダー*/
+  $('.slick-area').slick({
+    arrows: false,
+    centerMode: true,
+    centerPadding: '100px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: '50px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  /* scroll時のimgフェード表示 */
+  $(window).scroll(function() {
+    $('.fadein').each(function () {
+      let scroll = $(window).scrollTop();
+      let target = $(this).offset().top;
+      let windowHeight = $(window).height();
+
+      if(scroll > target - windowHeight + 200) {
+        $(this).css('opacity','1');
+        $(this).css('transform', 'translateY(0)');
+      }
+    });
+  });
 
 });
